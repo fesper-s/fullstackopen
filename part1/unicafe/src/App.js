98a1bf19a6231840884e30fb5465499038e23076
fesper-s <fabricio_esper@outlook.com>
2024-11-08 14:28:21 -0300

    Exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,22 +4,26 @@ const Button = ({ clickHandle, text }) => (
     <button onClick={clickHandle}>{text}</button>
 )
 
-const Statics = (props) => {
-    const sumOfAll = props.good + props.neutral + props.bad
+const StatisticsLine = ({ text, value }) => (
+    <div>
+        {text} {value}
+    </div>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+    const sumOfAll = good + neutral + bad
 
     if (sumOfAll === 0)
         return <p>No feedback given</p>
 
     return (
         <div>
-            <p>
-                good {props.good}<br />
-                neutral {props.neutral}<br />
-                bad {props.bad}<br />
-                all {sumOfAll}<br />
-                average {(props.good + props.bad * -1) / sumOfAll}<br />
-                positive {(props.good / sumOfAll) * 100} %
-            </p>
+            <StatisticsLine text={'good'} value={good} />
+            <StatisticsLine text={'neutral'} value={neutral} />
+            <StatisticsLine text={'bad'} value={bad} />
+            <StatisticsLine text={'all'} value={sumOfAll} />
+            <StatisticsLine text={'average'} value={(good + bad * -1) / sumOfAll} />
+            <StatisticsLine text={'positive'} value={(good / sumOfAll) * 100 + ' %'} />
         </div>
     )
 }
@@ -35,8 +39,8 @@ const App = () => {
             <Button clickHandle={() => setGood(good + 1)} text={'good'} />
             <Button clickHandle={() => setNeutral(neutral + 1)} text={'neutral'} />
             <Button clickHandle={() => setBad(bad + 1)} text={'bad'} />
-            <h1>statics</h1>
-            <Statics good={good} neutral={neutral} bad={bad}/>
+            <h1>statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
