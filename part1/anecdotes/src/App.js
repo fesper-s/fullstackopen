720fb86d0ba26043f9ca99f44af9153eff09f1cd
fesper-s <fabricio_esper@outlook.com>
2024-11-11 18:13:37 -0300

    Exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -31,10 +31,14 @@ const App = () => {
 
     return (
         <div>
+            <h1>Anecdote of the day</h1>
             <p>{anecdotes[selected]}</p>
             <p>has {votes[selected]} votes</p>
             <Button onClick={handleVotes} text='vote' />
             <Button onClick={handleNextAnecdote} text='next anecdote' />
+            <h1>Anecdote with most votes</h1>
+            <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+            <p>has {Math.max(...votes)} votes</p>
         </div>
     )
 }
