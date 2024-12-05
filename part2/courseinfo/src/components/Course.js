ea303801627737bdfdd3f71fc61ac7351da3bf78
fesper-s <fesper-s@student.42.rio>
2024-12-05 12:08:40 -0300

    Exercise 2.2

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -12,13 +12,18 @@ const Content = ({ parts }) => {
   return <div>{partsElements}</div>;
 };
 
-const Total = ({ sum }) => <p>Number of exercises{sum}</p>;
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return <strong>total of {total} exercises</strong>;
+};
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
